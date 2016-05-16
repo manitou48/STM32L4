77d45c7b4a73ba71d228768fb51501f104763693
manitou48 <manitou@home>
2016-05-16 09:32:22 -0400

    driftrtc

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
  80mhz STM32L476RE dragonfly sketches
 
+driftrtc.ino   check frequency of RTC crystal or LSI
+
 mem2mem.ino DMA memcpy()
 
 
---
+++ driftrtc.ino
@@ -0,0 +1,119 @@
+// dragonfly test version pre RTC software
+
+#define PRREG(x) Serial.print(#x" 0x"); Serial.println(x,HEX)
+
+#define RTC_TR_PM                            ((uint32_t)0x00400000)
+#define RTC_TR_HT                            ((uint32_t)0x00300000)
+#define RTC_TR_HU                            ((uint32_t)0x000F0000)
+#define RTC_TR_MNT                           ((uint32_t)0x00007000)
+#define RTC_TR_MNU                           ((uint32_t)0x00000F00)
+#define RTC_TR_ST                            ((uint32_t)0x00000070)
+#define RTC_TR_SU                            ((uint32_t)0x0000000F)
+#define RTC_TR_RESERVED_MASK    ((uint32_t)0x007F7F7F)
+#define RTC_ISR_INITF                        ((uint32_t)0x00000040)
+#define RTC_ISR_INIT                         ((uint32_t)0x00000080)
+#define RTC_INIT_MASK           ((uint32_t)0xFFFFFFFF)  
+
+
+uint8_t RTC_Bcd2ToByte(uint8_t Value)
+{
+  uint32_t tmp = 0;
+  tmp = ((uint8_t)(Value & (uint8_t)0xF0) >> (uint8_t)0x4) * 10;
+  return (tmp + (Value & (uint8_t)0x0F));
+}
+
+
+uint32_t rtc_ms() {
+		uint32_t ms, tmpreg;
+		uint8_t hrs,mins,secs,ss;
+
+    ss = RTC->SSR;  // subseconds /256 counting down, rollover issue
+		tmpreg = (uint32_t)(RTC->TR & RTC_TR_RESERVED_MASK);
+    RTC->DR;   // must read DR after TR 
+		hrs = (uint8_t)RTC_Bcd2ToByte((tmpreg & (RTC_TR_HT | RTC_TR_HU)) >> 16);
+		mins = (uint8_t)RTC_Bcd2ToByte((tmpreg & (RTC_TR_MNT | RTC_TR_MNU)) >>8);
+		secs = (uint8_t)RTC_Bcd2ToByte(tmpreg & (RTC_TR_ST | RTC_TR_SU));
+		tmpreg = 3600*hrs + 60*mins + secs;
+        ms = 1000*(tmpreg + (255-ss)/256.);
+		return ms;
+}
+
+void rtc_init() {
+	// from pyboard core
+	/* Disable the write protection for RTC registers */
+	RTC->WPR = 0xCA;
+	RTC->WPR = 0x53;
+	// set init mode
+	RTC->ISR = (uint32_t)RTC_INIT_MASK;
+	while((RTC->ISR &  RTC_ISR_INITF) == 0);  // ? timeout
+
+//  RTC->PRER = 0x00ff;
+//  RTC->PRER |= (0x007f)<<16;
+	RTC->DR = 0x162514;     // BCD calendar
+  RTC->TR = 0;
+
+	// exit init mode
+	RTC->ISR &= (uint32_t)~RTC_ISR_INIT;
+	//  enable write protection
+	RTC->WPR = 0xff;
+}
+
+
+#define RCC_LSI_ON 1
+#define RCC_LSI_RDY 2
+#define RCC_RTCCLKSOURCE_LSI ((uint32_t)0x00000200)
+
+void initLSI() {
+#if 0
+    // setup LSI for RTC use   no HAL support
+    PRREG(PWR->CR1);
+    PRREG(RCC->CSR);
+    PRREG(RCC->BDCR);
+    RCC->CSR |= RCC_LSI_ON;
+    while ((RCC->CSR & RCC_LSI_RDY ) ==0) ;
+
+        // Reset Backup domain
+    RCC->BDCR |= RCC_BDCR_BDRST;
+    RCC->BDCR &= ~RCC_BDCR_BDRST;
+
+    RCC->BDCR = (RCC->BDCR & ~RCC_BDCR_RTCSEL) | RCC_RTCCLKSOURCE_LSI;
+
+    PRREG(PWR->CR1);
+    PRREG(RCC->CSR);
+    PRREG(RCC->BDCR);
+ 
+#endif
+}
+
+
+uint32_t s0;
+void setup() {
+  Serial.begin(9600);
+  while(!Serial);
+  delay(3500);
+   PRREG(RCC->BDCR);
+   initLSI();
+   RCC->BDCR |= RCC_BDCR_RTCEN;   // enable RTC
+
+	 rtc_init();
+     PRREG(RTC->TR);
+     PRREG(RTC->DR);
+     PRREG(RTC->SSR);
+     PRREG(RTC->ISR);
+     PRREG(RTC->CR);
+     PRREG(RTC->PRER);
+     PRREG(RCC->BDCR);
+     delay(3000);
+     PRREG(RTC->TR);
+     PRREG(RTC->DR);
+     PRREG(RTC->SSR);
+     PRREG(RTC->ISR);  
+}
+
+void loop() {
+  uint32_t ms;
+
+		ms = rtc_ms();
+    Serial.println(ms);
+    delay(5000); 
+}
