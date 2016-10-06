fe9a4c93cb0054ed2f82a8929de8493956e145e9
manitou48 <manitou@home>
2016-10-06 08:12:39 -0400

    do alarm every n secconds

--- rtcalarm.ino
+++ rtcalarm.ino
@@ -7,6 +7,10 @@
 #define PRREG(x) Serial.print(#x" 0x"); Serial.println(x,HEX)
 
 #define RTC_TR_RESERVED_MASK    ((uint32_t)0x007F7F7F)
+#define BYTE2BCD(byte)      ((byte % 10) | ((byte / 10) << 4))
+#define BCD2BYTE(byte)      ((byte & 0x0f) + 10*((byte >> 4) & 0x0f))
+
+static int secsinc;  // interrupt every secsinc seconds
 
 volatile uint32_t ticks;
 
@@ -16,8 +20,21 @@ extern "C" void RTC_Alarm_IRQHandler(void) {
 		ticks++;
 		RTC->ISR &= (uint32_t)~RTC_ISR_ALRAF;   // clear wakeup
 		EXTI->PR1 |= EXTI_PR1_PIF18;
+		// update target time   ALRAWF already set
+		/* Disable the write protection for RTC registers */
+		RTC->WPR = 0xCA;
+		RTC->WPR = 0x53;
+		int secs = BCD2BYTE(RTC->ALRMAR);
+		int bcdsecs = BYTE2BCD((secs + secsinc)%60);
+  
+		RTC->CR &= ~(RTC_CR_ALRAIE |RTC_CR_ALRAE);   // clear 
+		while((RTC->ISR & RTC_ISR_ALRAWF)  == 0);   // wait
+		RTC->ALRMAR = RTC_ALRMAR_MSK4  | RTC_ALRMAR_MSK3 | RTC_ALRMAR_MSK2 | bcdsecs;
+		RTC->CR |= RTC_CR_ALRAE | RTC_CR_ALRAIE ;   // enable  ALARM A
+		RTC->WPR = 0xff; //  enable write protection
 	}
 }
+
 
 
 uint8_t RTC_Bcd2ToByte(uint8_t Value)
@@ -28,23 +45,22 @@ uint8_t RTC_Bcd2ToByte(uint8_t Value)
 }
 
 
-uint32_t rtc_ms() {
-		uint32_t ms, tmpreg;
-		uint8_t hrs,mins,secs,ss;
+uint32_t rtc_secs() {
+		uint32_t  tmpreg;
+		uint8_t hrs,mins,secs;
 
-    ss = RTC->SSR;  // subseconds /256 counting down, rollover issue
 		tmpreg = (uint32_t)(RTC->TR & RTC_TR_RESERVED_MASK);
-    RTC->DR;   // must read DR after TR 
+		RTC->DR;   // must read DR after TR 
 		hrs = (uint8_t)RTC_Bcd2ToByte((tmpreg & (RTC_TR_HT | RTC_TR_HU)) >> 16);
 		mins = (uint8_t)RTC_Bcd2ToByte((tmpreg & (RTC_TR_MNT | RTC_TR_MNU)) >>8);
 		secs = (uint8_t)RTC_Bcd2ToByte(tmpreg & (RTC_TR_ST | RTC_TR_SU));
 		tmpreg = 3600*hrs + 60*mins + secs;
-        ms = 1000*(tmpreg + (255-ss)/256.);
-		return ms;
+		return tmpreg;
 }
 
-void rtc_init() {
+void alarm_init(int secs) {
 	/* Disable the write protection for RTC registers */
+	secsinc = secs%60;
 	RTC->WPR = 0xCA;
 	RTC->WPR = 0x53;
 
@@ -56,12 +72,12 @@ void rtc_init() {
 
   RTC->CR &= ~(RTC_CR_ALRAIE |RTC_CR_ALRAE);   // clear 
   while((RTC->ISR & RTC_ISR_ALRAWF)  == 0);   // wait
-   // configure A ALARM 7th second every minute
-  RTC->ALRMAR = RTC_ALRMAR_MSK4  | RTC_ALRMAR_MSK3 | RTC_ALRMAR_MSK2 | 7 ;
+   // configure A ALARM  secsinc from now
+  int bcdsecs = BYTE2BCD((rtc_secs() + secsinc)%60);
+  RTC->ALRMAR = RTC_ALRMAR_MSK4  | RTC_ALRMAR_MSK3 | RTC_ALRMAR_MSK2 | bcdsecs;
   RTC->CR |= RTC_CR_ALRAE | RTC_CR_ALRAIE ;   // enable  ALARM A
 
-	//  enable write protection
-	RTC->WPR = 0xff;
+	RTC->WPR = 0xff; //  enable write protection
 
   __enable_irq();         //Alarm is set, so irqs can be enabled again
 
@@ -76,15 +92,16 @@ void setup() {
   while(!Serial);
   pinMode(13,OUTPUT);
   digitalWrite(13,HIGH);
-	rtc_init();
+	alarm_init(3);
 }
 
 void loop() {
-  uint32_t ms;
+  uint32_t secs;
   char str[128];
 
-  ms = rtc_ms();
-  sprintf(str,"%d ms %d ticks",ms,ticks);
+  secs = rtc_secs();
+  sprintf(str,"%d secs %d ticks",secs,ticks);
   Serial.println(str);
+
   delay(5000); 
 }
