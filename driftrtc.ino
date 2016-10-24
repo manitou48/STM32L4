fb3086e5a440d8a3d8e1e6ceba1eb7bb4fbb00f8
manitou48 <manitou@home>
2016-10-24 10:42:57 -0400

    set BYPSHAD, account for prescale setting

--- driftrtc.ino
+++ driftrtc.ino
@@ -2,16 +2,8 @@
 
 #define PRREG(x) Serial.print(#x" 0x"); Serial.println(x,HEX)
 
-#define RTC_TR_PM                            ((uint32_t)0x00400000)
-#define RTC_TR_HT                            ((uint32_t)0x00300000)
-#define RTC_TR_HU                            ((uint32_t)0x000F0000)
-#define RTC_TR_MNT                           ((uint32_t)0x00007000)
-#define RTC_TR_MNU                           ((uint32_t)0x00000F00)
-#define RTC_TR_ST                            ((uint32_t)0x00000070)
-#define RTC_TR_SU                            ((uint32_t)0x0000000F)
 #define RTC_TR_RESERVED_MASK    ((uint32_t)0x007F7F7F)
-#define RTC_ISR_INITF                        ((uint32_t)0x00000040)
-#define RTC_ISR_INIT                         ((uint32_t)0x00000080)
+
 #define RTC_INIT_MASK           ((uint32_t)0xFFFFFFFF)  
 
 
@@ -24,17 +16,20 @@ uint8_t RTC_Bcd2ToByte(uint8_t Value)
 
 
 uint32_t rtc_ms() {
-		uint32_t ms, tmpreg;
+		uint32_t ms, tmpreg, SSticks = (RTC->PRER & RTC_PRER_PREDIV_S) +1;
 		uint8_t hrs,mins,secs,ss;
 
-    ss = RTC->SSR;  // subseconds /256 counting down, rollover issue
-		tmpreg = (uint32_t)(RTC->TR & RTC_TR_RESERVED_MASK);
+  do {
+    ss = RTC->SSR;  // subseconds  counting down, rollover issue
+    tmpreg = (uint32_t)(RTC->TR & RTC_TR_RESERVED_MASK);
     RTC->DR;   // must read DR after TR 
+  } while(ss != RTC->SSR);
+
 		hrs = (uint8_t)RTC_Bcd2ToByte((tmpreg & (RTC_TR_HT | RTC_TR_HU)) >> 16);
 		mins = (uint8_t)RTC_Bcd2ToByte((tmpreg & (RTC_TR_MNT | RTC_TR_MNU)) >>8);
 		secs = (uint8_t)RTC_Bcd2ToByte(tmpreg & (RTC_TR_ST | RTC_TR_SU));
 		tmpreg = 3600*hrs + 60*mins + secs;
-        ms = 1000*(tmpreg + (255-ss)/256.);
+    ms = 1000*tmpreg + 1000*(SSticks-1 - ss)/SSticks;
 		return ms;
 }
 
@@ -47,21 +42,25 @@ void rtc_init() {
 	RTC->ISR = (uint32_t)RTC_INIT_MASK;
 	while((RTC->ISR &  RTC_ISR_INITF) == 0);  // ? timeout
 
-//  RTC->PRER = 0x00ff;
-//  RTC->PRER |= (0x007f)<<16;
+// default/reset prescale  256 ticks/sec
+  RTC->PRER = 0x00ff | (0x007f)<<16;
+//   RTC->PRER = RTC_PRER_PREDIV_S;     // 32768 ticks/sec
 	RTC->DR = 0x162514;     // BCD calendar
   RTC->TR = 0;
+  RTC->CR |= RTC_CR_BYPSHAD;   // need do-while if set ?
 
 	// exit init mode
 	RTC->ISR &= (uint32_t)~RTC_ISR_INIT;
 	//  enable write protection
 	RTC->WPR = 0xff;
+ 
 }
 
 
 #define RCC_LSI_ON 1
 #define RCC_LSI_RDY 2
 #define RCC_RTCCLKSOURCE_LSI ((uint32_t)0x00000200)
+#define RCC_RTCCLKSOURCE_LSE ((uint32_t)0x00000100)
 
 void initLSI() {
 #if 0
@@ -81,6 +80,12 @@ void initLSI() {
     PRREG(PWR->CR1);
     PRREG(RCC->CSR);
     PRREG(RCC->BDCR);
+#else
+  // re enable LSE
+   // RCC->BDCR |= RCC_BDCR_BDRST;
+   // RCC->BDCR &= ~RCC_BDCR_BDRST;
+
+   // RCC->BDCR = (RCC->BDCR & ~RCC_BDCR_RTCSEL) | RCC_RTCCLKSOURCE_LSE;
  
 #endif
 }
@@ -90,12 +95,14 @@ uint32_t s0;
 void setup() {
   Serial.begin(9600);
   while(!Serial);
+  pinMode(13,OUTPUT);
   delay(3500);
-   PRREG(RCC->BDCR);
+   //PRREG(RCC->BDCR);
    initLSI();
    RCC->BDCR |= RCC_BDCR_RTCEN;   // enable RTC
 
 	 rtc_init();
+#if 0
      PRREG(RTC->TR);
      PRREG(RTC->DR);
      PRREG(RTC->SSR);
@@ -103,17 +110,43 @@ void setup() {
      PRREG(RTC->CR);
      PRREG(RTC->PRER);
      PRREG(RCC->BDCR);
+     PRREG(RCC->CSR);
      delay(3000);
      PRREG(RTC->TR);
      PRREG(RTC->DR);
      PRREG(RTC->SSR);
      PRREG(RTC->ISR);  
+#endif
+}
+
+void logger() {
+  // check drift with hostdrift
+  static long cnt=0;
+  long ms;
+
+  while(Serial.available() < 4);   // wait for host request
+  ms = rtc_ms();
+  Serial.read();
+  Serial.read();
+  Serial.read();
+  Serial.read();
+  Serial.write((uint8_t *)&ms,4);
+  cnt++;
+  digitalWrite(13, cnt & 1);
+}
+
+void display() {
+  uint32_t ms;
+
+  while(1) {
+		ms = rtc_ms();
+    Serial.println(ms);
+    delay(5000);
+  } 
 }
 
 void loop() {
-  uint32_t ms;
-
-		ms = rtc_ms();
-    Serial.println(ms);
-    delay(5000); 
+  // logger();
+  display();
 }
+
